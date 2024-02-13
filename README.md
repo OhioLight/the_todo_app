6289f5b8d47921de354a43597e30408e3cd3352f
Kai Hatje <kai.hatje@app-akademie.com>
2024-02-13 21:47:24 +0100

    Initial Firebase setup.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A very simple todo app that doesn't work yet, because state is not being managed right.
 
 Can be fixed by passing state around or using a state management library like `provider`.
+
+This branch shows how to use Firebase / Firestore to store the todos.
+In order to uses it, you need to connect it to an appropriate Firebase project.
+
+This can be done by running `flutterfire configure --project=<project-name>`.
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:the_todo_app/todo_app.dart';
 import 'package:the_todo_app/todo_provider.dart';
+
+import 'firebase_options.dart';
 
 /// Schritte für SMS mit provider:
 /// - Schritt 0: Package provider einbinden :)
 /// - Schritt 1: Einen Provider erstellen.
 /// - Schritt 2: Den Provider für die App zur Verfügung stellen.
 /// - Schritt 3: Den Provider verwenden (Consumer, context.watch, ...).
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     // Schritt 2: Den Provider für die App zur Verfügung stellen.
     ChangeNotifierProvider(
